8138a591d60c9f27d143bd095d19d0e4b8f94166
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-04-03 21:06:32 +0200

    Added Home Page

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import phonebook from 'images/phone.png';
+import styles from './HomeView.module.scss';
+
+const HomeView = () => {
+  return (
+    <div className={styles.Container}>
+      <img src={phonebook} alt="phonebook" height="600" width="310" />
+
+      <div className={styles.Wraper}>
+        <h1 className={styles.Title}>Phonebook</h1>
+
+        <p className={styles.HomeText}>To start using our app, please</p>
+        <Link to="/login" className={styles.HomeLink}>
+          Log in
+        </Link>
+        <span className={styles.HomeSpan}>/</span>
+        <Link to="/register" className={styles.HomeLink}>
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomeView;
