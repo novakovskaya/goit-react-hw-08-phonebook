7dc32d8bb9209096f3362e748cf9813ae36f7f4e
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-04-03 21:02:57 +0200

    Added Contact List

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { contactsSelectors, contactsOperations } from 'redux/contacts';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import styles from './ContactList.module.scss';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(87, 115, 246)',
+    },
+  },
+});
+
+export const ContactList = () => {
+  const onFilteredContacts = useSelector(contactsSelectors.getFilteredContacts);
+  const dispatch = useDispatch();
+
+  const onDeleteContact = id => dispatch(contactsOperations.deleteContact(id));
+
+  return (
+    <ul className={styles.List}>
+      {onFilteredContacts.map(({ id, name, number }) => (
+        <li key={id} className={styles.Item}>
+          <p className={styles.Contact}>{name}:</p>
+          <p className={styles.Number}>{number}</p>
+
+          <ThemeProvider theme={theme}>
+            <Button
+              type="button"
+              className={styles.Button}
+              variant="contained"
+              size="small"
+              color="primary"
+              onClick={() => onDeleteContact(id)}
+            >
+              Delete
+            </Button>
+          </ThemeProvider>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  filteredContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
