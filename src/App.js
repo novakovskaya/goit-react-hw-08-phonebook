264636f5f5d6d1dc767c73b95fee49763a95ff65
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-04-17 13:08:34 +0200

    Update views

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React, { useEffect, Suspense } from 'react';
+import React, { lazy, useEffect, Suspense } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Switch } from 'react-router-dom';
 import { TailSpin } from 'react-loader-spinner';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import HomeView from './views/HomeView';
-import RegisterView from './views/RegisterView';
-import LoginView from './views/LoginView';
-import ContactsView from './views/ContactsView';
 import { Container } from './components/Container';
 import { AppBar } from 'components/AppBar';
 import { authOperations, authSelectors } from 'redux/auth';
 import { PublicRoute } from 'components/PublicRoute';
 import { PrivateRoute } from 'components/PrivateRoute';
 
-// const HomeView = lazy(() =>
-//   import('./views/HomeView' /* webpackChunkName: "home-view" */)
-// );
-// const RegisterView = lazy(() =>
-//   import('views/RegisterView' /* webpackChunkName: "register-view" */)
-// );
-// const LoginView = lazy(() =>
-//   import('views/LoginView' /* webpackChunkName: "login-view" */)
-// );
-// const ContactsView = lazy(() =>
-//   import('views/ContactsView' /* webpackChunkName: "contacts-view" */)
-// );
+const HomeView = lazy(() =>
+  import('./views/HomeView' /* webpackChunkName: "home-view" */)
+);
+const RegisterView = lazy(() =>
+  import('./views/RegisterView' /* webpackChunkName: "register-view" */)
+);
+const LoginView = lazy(() =>
+  import('./views/LoginView' /* webpackChunkName: "login-view" */)
+);
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' /* webpackChunkName: "contacts-view" */)
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -41,18 +37,18 @@ const App = () => {
       <AppBar />
 
       {!isFetchingCurrentUser && (
-        <Switch>
-          <Suspense
-            fallback={
-              <TailSpin
-                heigth="60"
-                width="60"
-                color="#5773f6"
-                ariaLabel="loading"
-              />
-            }
-          >
-            <PublicRoute exact path="/" restricted redirectTo="/contacts">
+        <Suspense
+          fallback={
+            <TailSpin
+              heigth="60"
+              width="60"
+              color="#5773f6"
+              ariaLabel="loading"
+            />
+          }
+        >
+          <Switch>
+            <PublicRoute exact path="/" redirectTo="/contacts">
               <HomeView />
             </PublicRoute>
 
@@ -67,8 +63,8 @@ const App = () => {
             <PrivateRoute path="/contacts" redirectTo="/login">
               <ContactsView />
             </PrivateRoute>
-          </Suspense>
-        </Switch>
+          </Switch>
+        </Suspense>
       )}
       <ToastContainer autoClose={1500} />
     </Container>
